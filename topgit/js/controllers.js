88c3c868b41fca9320a601771519ed23c55a654f
steady-daddy <thisismansoorkhan@gmail.com>
2016-09-05 14:24:33 +0530

    improved code quality, added null checks

--- topgit/js/controllers.js
+++ topgit/js/controllers.js
@@ -4,6 +4,7 @@ appControllers.controller('DataController', ['$scope', '$rootScope', '$http', fu
 
     $rootScope.foundResults = false;
     $rootScope.slider = {};
+    $scope.items = [];
     NProgress.start();
     var paginateLink = false;
 
@@ -12,24 +13,24 @@ appControllers.controller('DataController', ['$scope', '$rootScope', '$http', fu
     });
 
     //For debugging/testing use local data
-    //$http.get('data/sample.json',{cache:true}).success(function(response) {
-    //    setData(response);
-    //});
+    $http.get('data/sample.json',{cache:true}).success(function(response) {
+        setData(response);
+    });
 
-    var url = "https://api.github.com/search/repositories?q=stars:>1&s=stars&type=Repositories&callback=JSON_CALLBACK";
-    $http.jsonp(url,{cache: true})
-        .success(function(response){
-            $rootScope.rateLimit = response.meta['X-RateLimit-Limit'];
-            $rootScope.rateLimitRemaining = response.meta['X-RateLimit-Remaining'];
-            var nextPage = response.meta['Link'][0];
-            nextPage = nextPage[0].split(",");
-            paginateLink = nextPage[0];
-            paginateLink = paginateLink.replace('angular.callbacks._0', 'JSON_CALLBACK')
-            setData(response.data);
-        });
+    //var url = "https://api.github.com/search/repositories?q=stars:>1&s=stars&type=Repositories&callback=JSON_CALLBACK";
+    //$http.jsonp(url,{cache: true})
+    //    .success(function(response){
+    //        setData(response);
+    //    });
 
-    setData = function (response) {
-        $scope.items = response.items;
+    function setData(response, flag) {
+
+        if($scope.items == null){
+            $scope.items = response.data.items
+        }else{
+            $scope.items.push.apply($scope.items, response.data.items);
+        }
+
         $scope.min = min = Math.min.apply(Math,$scope.items.map(function(item){return item.stargazers_count;}));
         $scope.max = max = Math.max.apply(Math,$scope.items.map(function(item){return item.stargazers_count;}));
         $rootScope.slider = {
@@ -39,15 +40,24 @@ appControllers.controller('DataController', ['$scope', '$rootScope', '$http', fu
                 ceil: max
             }
         };
-        $rootScope.recordsCount = response.total_count;
-        NProgress.done();
 
+        $rootScope.recordsCount = response.data.total_count;
+        if(response.hasOwnProperty('meta')){
+            $rootScope.rateLimit = response.meta['X-RateLimit-Limit'];
+            $rootScope.rateLimitRemaining = response.meta['X-RateLimit-Remaining'];
+            var nextPage = response.meta['Link'][0];
+            nextPage = nextPage[0].split(",");
+            paginateLink = nextPage[0];
+            paginateLink = paginateLink.replace(/angular.callbacks._[0-9]/, 'JSON_CALLBACK');
+        }
+
+        NProgress.done();
     };
 
     $scope.searchRepos = function(){
         var url = "https://api.github.com/search/repositories?q="+$scope.selLang+"&sort=language&type=Repositories&callback=JSON_CALLBACK";
+        $scope.items = null;
         fetchRepos(url);
-        $rootScope.foundResults = true;
         $rootScope.searchTerm = $scope.selLang;
     };
 
@@ -76,11 +86,12 @@ appControllers.controller('DataController', ['$scope', '$rootScope', '$http', fu
         fetchRepos(paginateLink);
     };
 
-    fetchRepos = function (url){
+    function fetchRepos(url){
         NProgress.start();
         $http.jsonp(url, {cache: true})
             .success(function(response){
-                setData(response.data);
+                setData(response, true);
+                $rootScope.foundResults = true;
             });
     }
 }]);
